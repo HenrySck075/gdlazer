b0a88d9092d9010486186c08c72513db199a5c06
chikawaiii <email.is.not.vaild9@gmail.com>
2025-09-04 15:54:50 +0700

    ok man

--- src/widgets/Element.hpp
+++ src/widgets/Element.hpp
@@ -1,5 +1,22 @@
 #pragma once
 
-class Element {
+#include "Geode/cocos/base_nodes/CCNode.h"
+#include "shared_ptr_2.hpp"
+#include <memory>
 
+class RenderElement;
+
+class Widget;
+class Element;
+using ElementPtr = std::shared_ptr<Element>;
+
+class Element {
+private:
+  RenderElement* getAncestorRenderElement();
+  std::shared_ptr<Widget*> m_widget;
+
+public:
+  Element(shared_ptr_ctor<Widget> widget) : m_widget(widget) {};
+
+  virtual void attachRender();
 }; 
--- src/widgets/Widget.hpp
+++ src/widgets/Widget.hpp
@@ -1,10 +1,14 @@
 #pragma once
 #include "shared_ptr_2.hpp"
 
+
+struct Element;
 struct Widget;
 using WidgetPtr = shared_ptr_ctor<Widget>;
 
 struct Widget {
     std::string id;
     std::string classes;
+
+    virtual Element* createElement();
 };
